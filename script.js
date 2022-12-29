4eafa49499e5b1debd9067b1f71418c6c1ad622f
Quvonchbek048 <quvonchbeku048@gmail.com>
2022-12-29 13:00:40 +0500

    Team Quvonchbek

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
-// let a = +prompt("Birinchi sonni kiriting")
-// let b = +prompt("Ikkinchi sonni kiriting")
-// let c = prompt("Amalni tanlang + - * / %")
+let name = prompt('Ismingizni kiriting');
 
 
+    alert("Salom " + name + " bu veb-sayt orqali siz misollarni hisoblashingiz mumkin")
 
-// let x = prompt("Raqamni kiriting")
-
-// if (x % 2 == 0) {
-//     console.log("bu raqam juft");
-//     alert("bu raqam juft")
-// }
-// else {
-//     console.log("bu raqam toq");
-//     alert("bu raqam toq")
-// }
-
-
-// ------------------------------------------------------------------------------------------------------------------------- //
-
-
-
-
+// ----------------------------------------------------------------------------------------------------------------- //
 function calc() {
     let n1 = +document.getElementById('n1').value;
     let n2 = +document.getElementById('n2').value;
@@ -28,15 +11,15 @@ function calc() {
     let result = document.getElementById('result').value;
 
     if (operators === '+') {
-        let result = document.getElementById('result').value = n1+n2;
+        let result = document.getElementById('result').value = n1 + n2;
     }
     else if (operators === '-') {
-        let result = document.getElementById('result').value = n1-n2;
+        let result = document.getElementById('result').value = n1 - n2;
     }
     else if (operators === '×') {
-        let result = document.getElementById('result').value = n1*n2;
+        let result = document.getElementById('result').value = n1 * n2;
     }
     else if (operators === ':') {
-        let result = document.getElementById('result').value = n1/n2;
+        let result = document.getElementById('result').value = n1 / n2;
     }
 }
